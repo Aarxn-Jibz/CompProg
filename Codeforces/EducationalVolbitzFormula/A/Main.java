cf1affbdaa2834e4e109e7fa49e310517cbfa732
Aaron Jibin <aaron.jibin@btech.christuniversity.in>
2025-11-29 21:17:01 +0530

    Did codeforces educational volbitz A

---
+++ Codeforces/EducationalVolbitzFormula/A/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double n = sc.nextDouble();
+    System.out.println("25");
+  }
+}
