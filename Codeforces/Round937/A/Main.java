7042244f309da808484c9015122f5fbab37afec8
Aaron Jibin <aaron.jibin@btech.christuniversity.in>
2025-12-09 22:10:24 +0530

    POTATO

---
+++ Codeforces/Round937/A/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for (int i = 0; i < t; i++) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      if (b > a && b < c) {
+        System.out.println("STAIR");
+      } else if (b > a && b > c) {
+        System.out.println("PEAK");
+      } else {
+        System.out.println("NONE");
+      }
+    }
+  }
+}
