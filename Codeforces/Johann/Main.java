3119f8d5165988abd004ab0eb201704273c22111
Aaron Jibin <aaron.jibin@btech.christuniversity.in>
2025-12-16 21:03:52 +0530

    Johan fun

---
+++ Codeforces/Johann/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("I need something in my mouth!");
+    System.out.println("I like having physical contact!");
+    System.out.println(" - Johann(2025)");
+  }
+}
